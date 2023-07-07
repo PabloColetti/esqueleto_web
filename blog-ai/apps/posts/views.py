b1891e7ef1ccb8fef0d4266eaa27e6046241db64
Pablo Coletti <pablo01po011@gmail.com>
2023-07-07 19:58:06 -0300

    include urls posts

--- blog-ai/apps/posts/views.py
+++ blog-ai/apps/posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def lista_posts(request):
+    posts = ['GPT-4', 'AutoGPT', 'AI Open Source']
+    context = {
+        'posts': posts
+    }
+    return render(request, 'posts.html', context)
