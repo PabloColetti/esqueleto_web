b486f37d4330d4a2b5260740d23695ec31c2f959
Pablo Coletti <pablo01po011@gmail.com>
2023-07-22 16:19:52 -0300

    agregamos template post

--- blog-ai/blog/configuraciones/settings.py
+++ blog-ai/blog/configuraciones/settings.py
@@ -72,21 +72,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'blog.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pruebas_django',
-        'USER': 'root', 
-        'PASSWORD': 'facil123',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
